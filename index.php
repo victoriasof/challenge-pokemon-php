8f7b57f17a199883477e340eb7237351d9432b82
Victoria <victoria.sofianidou@gmail.com>
2020-10-01 12:26:59 +0200

    added random moves with count, array_rand and array_map

--- index.php
+++ index.php
@@ -29,11 +29,28 @@ if (isset($_GET['pokemonId']) && $_GET['pokemonId'] !== '') {
     $data = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $inputVal);
     $dataArr = json_decode($data, true);
 
-    
     $pokeId = $dataArr['id'];
     $pokeName =$dataArr['name'];
     $pokeImage =$dataArr['sprites']['front_default'];
+
     $pokeMoves = $dataArr['moves'];
+
+    $count = count($dataArr['moves']);
+
+    if ($count > 3) {
+        $max = 4;
+    }else {
+        $max = $count;
+    }
+
+    $randomArrayIndexes = array_rand($dataArr['moves'], $max);
+    // array_map loops through each array item and uses the current array item as a function parameter
+    // and makes a new array containing the returned values of the function
+    $moves = array_map(function($index) {
+        global $dataArr; // global keyword allows us to access global variables that are initialize outside of the function
+        return $dataArr['moves'][$index]['move']['name'];
+    }, $randomArrayIndexes);
+
 
     $speciesUrl = $dataArr['species']['url'];
     $speciesDataArr = json_decode(file_get_contents($speciesUrl), true);
